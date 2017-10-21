921db962ec398caa46716a457927075fc040f3f9
Simon Brand <tartanllama@gmail.com>
2017-10-21 22:03:49 +0100

    Remove unused aliases

--- optional.hpp
+++ optional.hpp
@@ -48,41 +48,14 @@ template <class T> class optional;
 
 /// \exclude
 namespace detail {
-template <class T> using remove_cv_t = typename std::remove_cv<T>::type;
 template <class T> using remove_const_t = typename std::remove_const<T>::type;
 template <class T>
-using remove_volatile_t = typename std::remove_volatile<T>::type;
-template <class T> using add_cv_t = typename std::add_cv<T>::type;
-template <class T> using add_const_t = typename std::add_const<T>::type;
-template <class T> using add_volatile_t = typename std::add_volatile<T>::type;
-template <class T>
 using remove_reference_t = typename std::remove_reference<T>::type;
-template <class T>
-using add_lvalue_reference_t = typename std::add_lvalue_reference<T>::type;
-template <class T>
-using add_rvalue_reference_t = typename std::add_rvalue_reference<T>::type;
-template <class T>
-using remove_pointer_t = typename std::remove_pointer<T>::type;
-template <class T> using add_pointer_t = typename std::add_pointer<T>::type;
-template <class T> using make_signed_t = typename std::make_signed<T>::type;
-template <class T> using make_unsigned_t = typename std::make_unsigned<T>::type;
-template <class T> using remove_extent_t = typename std::remove_extent<T>::type;
-template <class T>
-using remove_all_extents_t = typename std::remove_all_extents<T>::type;
-template <std::size_t N, std::size_t A = N>
-using aligned_storage_t = typename std::aligned_storage<N, A>::type;
-template <std::size_t N, class... Ts>
-using aligned_union_t = typename std::aligned_union<N, Ts...>::type;
 template <class T> using decay_t = typename std::decay<T>::type;
 template <bool E, class T = void>
 using enable_if_t = typename std::enable_if<E, T>::type;
 template <bool B, class T, class F>
 using conditional_t = typename std::conditional<B, T, F>::type;
-template <class... Ts>
-using common_type_t = typename std::common_type<Ts...>::type;
-template <class T>
-using underlying_type_t = typename std::underlying_type<T>::type;
-template <class T> using result_of_t = typename std::result_of<T>::type;
 
 template <class...> struct conjunction : std::true_type {};
 template <class B> struct conjunction<B> : B {};
