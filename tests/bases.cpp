26c00afe274974c85d6e2129b9bd3a022bb169c1
Simon Brand <tartanllama@gmail.com>
2017-11-27 15:08:34 +0000

    Fix bases test

--- tests/bases.cpp
+++ tests/bases.cpp
@@ -4,6 +4,8 @@
 // Old versions of GCC don't have the correct trait names. Could fix them up if needs be.
 #if (defined(__GNUC__) && __GNUC__ == 4 && __GNUC_MINOR__ <= 9 &&              \
      !defined(__clang__))
+// nothing for now
+#else
 TEST_CASE("Triviality", "[bases.triviality]") {
     REQUIRE(std::is_trivially_copy_constructible<tl::optional<int>>::value);
     REQUIRE(std::is_trivially_copy_assignable<tl::optional<int>>::value);
