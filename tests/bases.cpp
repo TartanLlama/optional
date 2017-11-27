4308924a382218dbacca701dc6fcb50795340de4
Simon Brand <tartanllama@gmail.com>
2017-11-27 14:51:32 +0000

    Fix GCC issues

--- tests/bases.cpp
+++ tests/bases.cpp
@@ -1,6 +1,9 @@
 #include "catch.hpp"
 #include "optional.hpp"
 
+// Old versions of GCC don't have the correct trait names. Could fix them up if needs be.
+#if (defined(__GNUC__) && __GNUC__ == 4 && __GNUC_MINOR__ <= 9 &&              \
+     !defined(__clang__))
 TEST_CASE("Triviality", "[bases.triviality]") {
     REQUIRE(std::is_trivially_copy_constructible<tl::optional<int>>::value);
     REQUIRE(std::is_trivially_copy_assignable<tl::optional<int>>::value);
@@ -101,6 +104,5 @@ TEST_CASE("Deletion", "[bases.deletion]") {
         //REQUIRE(!std::is_move_constructible<tl::optional<T>>::value);
         //REQUIRE(!std::is_move_assignable<tl::optional<T>>::value);
     }
-
-
 }
+#endif
